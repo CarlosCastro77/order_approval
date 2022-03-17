a30ce2dce7fc302205c960e0eb8e13a474773439
Carlos Eduardo Castro de Carvalho <carloscastrodev@gmail.com>
2022-03-16 22:47:28 -0300

    ensure LoginPage presents error if email is invalid

---
+++ order_approval/test/ui/pages/login/login_page_test.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mocktail/mocktail.dart';
+
+enum UIError {
+  requiredField,
+  invalidField,
+}
+
+extension UIErrorExtension on UIError {
+  String get description {
+    switch(this) {
+      case UIError.requiredField: return 'Campo obrigatório';
+      case UIError.invalidField: return 'Campo inválido';
+    }
+  }
+}
+
+abstract class LoginPresenter implements Listenable {
+  Stream<UIError?> get emailErrorStream;
+}
+
+class LoginPresenterSpy extends Mock implements LoginPresenter {
+  final emailErrorController = StreamController<UIError?>();
+
+  LoginPresenterSpy() {
+    when(() => emailErrorStream).thenAnswer((_) => emailErrorController.stream);
+  }
+
+  void emitEmailError(UIError error) => emailErrorController.add(error);
+
+  void dispose() {
+    emailErrorController.close();
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  final LoginPresenter presenter;
+
+  const LoginPage(this.presenter);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            StreamBuilder<UIError?>(
+              stream: presenter.emailErrorStream,
+              builder: (context, snapshot) {
+                return TextFormField(
+                  decoration: InputDecoration(
+                    label: const Text('Email'),
+                    errorText: snapshot.data?.description
+                  )
+                );
+              }
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Senha')
+              )
+            ),
+            ElevatedButton(
+              onPressed: () {}, 
+              child: const Text('Entrar')
+            ),
+            TextButton(
+              onPressed: () {}, 
+              child: const Text('Esqueceu sua senha?')
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  late LoginPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = LoginPresenterSpy();
+    final loginPage = MaterialApp(home: LoginPage(presenter));
+    await tester.pumpWidget(loginPage);
+  }
+  
+  testWidgets('Should render with correct initial state', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    expect(find.byType(TextFormField), findsNWidgets(2));
+    expect(find.byType(ElevatedButton), findsOneWidget);
+    expect(find.byType(TextButton), findsOneWidget);
+
+    expect(find.text('Email'), findsOneWidget);
+    expect(find.text('Senha'), findsOneWidget);
+    expect(find.text('Entrar'), findsOneWidget);
+    expect(find.text('Esqueceu sua senha?'), findsOneWidget);
+  });
+
+  testWidgets('Should present error if email is invalid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    presenter.emitEmailError(UIError.invalidField);
+    await tester.pump();
+
+    expect(find.text('Campo inválido'), findsOneWidget);
+  });
+}
