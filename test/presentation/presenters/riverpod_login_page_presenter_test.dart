d9f66b89af70c3f3b34a03618ecfdcdd87fd16eb
Carlos Eduardo Castro de Carvalho <carloscastrodev@gmail.com>
2022-03-23 22:17:46 -0300

    ensure RiverpodLoginPagePresenter calls Validation with correct email

--- test/presentation/presenters/riverpod_login_page_presenter_test.dart
+++ test/presentation/presenters/riverpod_login_page_presenter_test.dart
@@ -1,13 +1,37 @@
 import 'package:test/test.dart';
 
-class RiverpodLoginPagePresenter {
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
 
+class RiverpodLoginPagePresenter {
+  final Validation validation;
+
+  RiverpodLoginPagePresenter({
+    required this.validation
+  });
+
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
 }
+
+abstract class Validation {
+  void validate({
+    required String field,
+    required String value
+  });
+}
+
+class ValidationSpy extends Mock implements Validation {}
 
 void main() {
   test('Should call Validation with correct email', () async {
-    final sut = RiverpodLoginPagePresenter();
+    final Validation validation = ValidationSpy();
+    final RiverpodLoginPagePresenter sut = RiverpodLoginPagePresenter(validation: validation);
+    final String fakeEmail = faker.internet.email();
 
-    
+    sut.validateEmail(fakeEmail);
+
+    verify(() => validation.validate(field: 'email', value: fakeEmail)).called(1);
   });
 }
